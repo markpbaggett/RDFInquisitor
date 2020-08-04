d0173ba748a67288aa207d50c9b7d22c2f6e8001
Mark Baggett <mbagget1@utk.edu>
2020-08-03 20:30:14 -0400

    Black things.

--- run.py
+++ run.py
@@ -4,12 +4,39 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Question your Remote RDF")
-    parser.add_argument("-u", "--uri", dest='uri', help="URI to negotiate", required=True)
-    parser.add_argument("-o", "--operation", dest='operation', help="Operation you want to run: download_rdf, get_labels, get_label_by_lang, get_types, get_range", required=True)
-    parser.add_argument("-f", "--file_format", dest='file_format', help="When downloading rdf, specify how you want to serialize things.", default='ttl')
-    parser.add_argument("-p", "--file_path", dest='file_path', help="When downloading rdf, where you want to serialize to disk without extension", default='rdf')
-    parser.add_argument("-s", "--subject", dest='subject', help="Specify a subject.  See docs for details.")
-    parser.add_argument("-x", "--extra", dest='extra', help="Specify an extra.  See docs for details.")
+    parser.add_argument(
+        "-u", "--uri", dest="uri", help="URI to negotiate", required=True
+    )
+    parser.add_argument(
+        "-o",
+        "--operation",
+        dest="operation",
+        help="Operation you want to run: download_rdf, get_labels, get_label_by_lang, get_types, get_range",
+        required=True,
+    )
+    parser.add_argument(
+        "-f",
+        "--file_format",
+        dest="file_format",
+        help="When downloading rdf, specify how you want to serialize things.",
+        default="ttl",
+    )
+    parser.add_argument(
+        "-p",
+        "--file_path",
+        dest="file_path",
+        help="When downloading rdf, where you want to serialize to disk without extension",
+        default="rdf",
+    )
+    parser.add_argument(
+        "-s",
+        "--subject",
+        dest="subject",
+        help="Specify a subject.  See docs for details.",
+    )
+    parser.add_argument(
+        "-x", "--extra", dest="extra", help="Specify an extra.  See docs for details."
+    )
     args = parser.parse_args()
 
     x = RDFInquisitor(args.uri)
