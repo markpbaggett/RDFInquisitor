6c9182d12e6b3b9c1648a81a0d0da308e4c00de7
Mark Baggett <mbagget1@utk.edu>
2020-08-12 12:51:48 -0400

    Echo Starting Flask.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+import sys
 
 app = Flask(__name__)
 app.config.from_object('config')
 bootstrap = Bootstrap(app)
+
+print("Starting Flask")
+sys.stdout.flush()
 
 from flaskr import views
