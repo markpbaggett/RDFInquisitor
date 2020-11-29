fe7bbcec216943d5862da21ab8e4ca9fdc1e9a12
Mark Baggett <mbagget1@utk.edu>
2020-11-29 08:20:51 -0500

    Store exit value of previous command.

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -3,6 +3,7 @@
 echo "Running pre-commit hooks."
 ./hooks/run-tests.sh
 
+# Store exit value from command above and check if it's not zero.
 if [ $? -ne 0 ]; then
  echo "Tests must pass before commit!"
  exit 1
