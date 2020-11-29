6f09748761d7d4c8bfc81c9751adab85fb6f84d3
Mark Baggett <mbagget1@utk.edu>
2020-11-29 08:23:08 -0500

    Ensure we're always at the root of our application.

--- hooks/run-tests.sh
+++ hooks/run-tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# if any command returns error, exit and return the error
 set -e
 
+# always make sure we're at the root of our application.
 cd "${0%/*}/.."
 
 echo "Running tests"
