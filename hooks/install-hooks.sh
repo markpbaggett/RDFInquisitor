987342713306583b4090beda3398ef349ac922e7
Mark Baggett <mbagget1@utk.edu>
2020-11-29 08:17:58 -0500

    Fix typo in symlink.

--- hooks/install-hooks.sh
+++ hooks/install-hooks.sh
@@ -4,7 +4,7 @@ GIT_DIR=$(git rev-parse --git-dir)
 PATH_TO_OUTPUT=$(pwd)
 
 echo "Installing hooks..."
-# this command creates symlink to our pre-commit script
-ln -s $PATH_TO_OUTPUT/hooks/pre-commit.sh $GIT_DIR/hooks/pre-commit2
+# Symlink precommit in git repo to .git hooks
+ln -s $PATH_TO_OUTPUT/hooks/pre-commit.sh $GIT_DIR/hooks/pre-commit
 echo "Done!"
 
