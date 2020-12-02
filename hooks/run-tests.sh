3f3ab118dbd0c10de85ea3de0ee91d225c60b816
Mark Baggett <mbagget1@utk.edu>
2020-12-01 19:40:44 -0500

    Only run tests on python changes.

--- hooks/run-tests.sh
+++ hooks/run-tests.sh
@@ -6,5 +6,23 @@ set -e
 # always make sure we're at the root of our application.
 cd "${0%/*}/.."
 
-echo -e "\nRunning tests:\n"
-pipenv run pytest inquisitor/question.py --doctest-modules
+ADDED_FILES=$(git --no-pager diff --cached --name-only | cat)
+TESTS=0
+
+# Determine if tests should run.
+for i in $ADDED_FILES
+do
+  if [ "${i: -3}" == ".py" ]
+  then
+    TESTS=1
+  fi
+done
+
+# Run tests.
+if [ "$TESTS" == 1 ]
+then
+  echo -e "\nRunning tests:\n"
+  pipenv run pytest inquisitor/question.py --doctest-modules
+else
+  echo -e "\nNo tests to run.  Skipping."
+fi
